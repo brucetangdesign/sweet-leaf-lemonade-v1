29398c084090d76c8e726bcab7292a318aae12d2
squeaky-bruce <bruce@squeaky.com>
2019-04-18 15:28:11 -0400

    copy update

--- index.php
+++ index.php
@@ -69,86 +69,12 @@ $page = "home-page";
     <?php include 'header.php'; ?>
     <!-- Backgrounds -->
     <section id="bg">
-      <!--
-      <div class="bg-gradient1">
-        <div class="floral-bg"></div>
-        <div class="floral-bg reverse"></div>
-      </div>
-      <div class="bg-gradient2 hidden">
-      </div>
-      <div class="bg-gradient3 hidden">
-        <div class="lights"></div>
-        <div class="lights reverse"></div>
-        <div class="skyline"></div>
-      </div>
-    -->
       <div class="bg-gradient4">
         <div class="texture-top"></div>
         <div class="nyc-bg"></div>
       </div>
     </section>
 
-    <!-- Main Slides -->
-    <!--
-    <section class="slide-main slide1">
-      <div class="bg-graphics" data-rellax-speed="-4"></div>
-      <div class="mint-fruit rellax" data-rellax-speed="2"></div>
-      <div class="bottle peach rellax" data-rellax-speed="-5"></div>
-      <div class="bottle honey rellax"></div>
-      <div class="bg-lemon rellax" data-rellax-speed="1"></div>
-      <div class="bg-orange rellax" data-rellax-speed="3"></div>
-      <div class="bottle mint rellax" data-rellax-speed="1"></div>
-      <div class="center"></div>
-      <div class="slide-content">
-        <div class="slide-image-container" data-rellax-speed="3">
-          <div class="slide-image"></div>
-        </div>
-        <div class="slide-copy" data-rellax-speed="3">
-          <h1>My family’s original tea recipe just for you.</h1>
-          <p class="subtitle">Organic from day one... Sweet but not too sweet, y’all best believe!</p>
-        </div>
-      </div>
-    </section>
-
-    <section class="slide-main slide2">
-      <div class="bg-graphics rellax" data-rellax-speed="-3"></div>
-      <div class="organic-stamp rellax" data-rellax-speed="5"></div>
-      <div class="center"></div>
-      <div class="slide-content">
-        <div class="slide-image-container" data-rellax-speed="3">
-          <div class="slide-image"></div>
-          <div class="slide-image glow"></div>
-        </div>
-        <div class="slide-copy rellax" data-rellax-speed="2">
-          <h1>Organic Sweet Leaf Tea! We wouldn't make it any other way!</h1>
-          <p class="subtitle">Our hearts have been in it 20+ years! Times change but the love in each bottle don’t! We use ingredients you can pronounce like real cane sugar! We reckon it’s not the way of most, but it sure as heck is the way we’re gonna keep doin’ it!<br>LOVE, Mimi</p>
-          <a class="button" href="about.php#our-products">Learn More</a>
-        </div>
-      </div>
-    </section>
-
-    <section class="slide-main slide3">
-      <div class="bg-graphics rellax" data-rellax-speed="-2"></div>
-      <div class="i-love-you rellax" data-rellax-speed="3"></div>
-      <div class="bottle-cap rellax" data-rellax-speed="1"></div>
-      <div class="austin-stamp rellax" data-rellax-speed="2"></div>
-      <div class="est-1998 rellax" data-rellax-speed="5.4"></div>
-      <div class="cactus rellax" data-rellax-speed="2"></div>
-      <div class="cactus reverse"></div>
-      <div class="center"></div>
-      <div class="slide-content">
-        <div class="slide-image-container" data-rellax-speed="1">
-          <div class="slide-image"></div>
-          <div class="slide-image glow"></div>
-        </div>
-        <div class="slide-copy rellax" data-rellax-speed="-2">
-          <h1>Special places brew special flavor.</h1>
-          <p class="subtitle">Born in Austin in Mimi’s kitchen, our original recipe of organic sweet tea still has that flavor only found ‘round here.</p>
-          <a class="button" href="about.php">Our Story</a>
-        </div>
-      </div>
-    </section>
--->
     <section id="about-products" class="slide-main slide4">
       <div class="product-clouds-left rellax" data-rellax-speed="-8"></div>
       <div class="product-left-blue-mango rellax" data-rellax-speed="-2"></div>
@@ -158,7 +84,7 @@ $page = "home-page";
       <div class="product-clouds-right rellax" data-rellax-speed="3"></div>
       <div class="about-products-main">
         <h1>What's both sour and sweet all at once? Our new organic lemonades for y'all and y'alls taste.</h1>
-        <p class="subtitle">Fpo lorem ipsum dolor ingredients copy lorem ipsum dolor copy. Fpo lorem ipsum dolor ingredients copy ipsum.</p>
+        <p class="subtitle">Just like Sweet Leaf Organic Iced Teas, our Lemonades are made with simple ingredients that you can pronounce, like organic lemon juice and organic cane sugar, along with a whole lotta love. Try one of our four flavors today! Sweet but not too sweet.</p>
         <div class="about-products-main-image"></div>
         <!--<div class="center"></div>-->
       </div>
